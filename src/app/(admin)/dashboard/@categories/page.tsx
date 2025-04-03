569f83184c6c659ca175a9c120db84245f6c2234
John_Konstantin <overdose.ut@gmail.com>
2025-04-03 22:44:49 +0300

    add mockapi api, env, fetch stats on dashboard

--- src/app/(admin)/dashboard/@categories/page.tsx
+++ src/app/(admin)/dashboard/@categories/page.tsx
@@ -1,25 +1,29 @@
 import { FC } from 'react';
-import { getSummaryCategories } from '@/lib/api';
+import { getCategories, getCompanies } from '@/lib/api';
+import getCountById from '@/lib/utils/getCountById';
 import StatCard, { StatCardType } from '@/app/components/stat-card';
 import DashboardCard from '@/app/components/dashboard-card';
 
 export interface PageProps {}
 
 const Page: FC<PageProps> = async ({}) => {
-  const data = await getSummaryCategories();
+  const categories = await getCategories();
+  const companies = await getCompanies();
+
+  const counts = getCountById(companies, 'categoryId');
 
   return (
     <DashboardCard label="Categories of companies">
       <div className="grid grid-cols-12 gap-3 pb-5 px-5 ">
-        {data.map(({ categoryId, categoryTitle, count }) => (
+        {categories.map(({ id, title }) => (
           <div
-            key={categoryId}
+            key={id}
             className="col-span-3 [&>*]: odd:text-purple-200 even:text-lime-200"
           >
             <StatCard
               type={StatCardType.Dark}
-              label={categoryTitle}
-              counter={count}
+              label={title}
+              counter={counts[id] || 0}
             />
           </div>
         ))}
--- src/app/(admin)/dashboard/@stats/page.tsx
+++ src/app/(admin)/dashboard/@stats/page.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { getSummaryStats } from '@/lib/api';
+import { getSummaryStats, SummaryStats } from '@/lib/api';
 import StatCard, { StatCardType } from '@/app/components/stat-card';
 
 export interface PageProps {}
@@ -12,11 +12,15 @@ const labelByStat = {
 };
 
 const Page: FC<PageProps> = async ({}) => {
-  const data = await getSummaryStats();
+  const data = await getSummaryStats({
+    next: {
+      revalidate: 5,
+    },
+  });
 
   return (
     <div className="grid grid-cols-12 gap-5">
-      {(Object.keys(labelByStat) as (keyof typeof data)[]).map(key => (
+      {(Object.keys(labelByStat) as (keyof SummaryStats)[]).map(key => (
         <div key={key} className="col-span-3">
           <StatCard
             type={StatCardType.Gradient}
